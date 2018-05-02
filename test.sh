05adcf82c4e32848b459edd722f74f0a816ce07c
WoodNeck <saiyu915@naver.com>
2018-05-02 12:24:49 +0900

    fix: Granting permission to test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'devel_fe' ]
+if [[ $TRAVIS_BRANCH == 'devel_fe' ]]
 then
   npm run test_fe
 elif [[ $TRAVIS_BRANCH == 'devel_be' ]]
 then
   python manage.py test
 else
-then
   npm run test_all
 fi
