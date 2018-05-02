6c8aac56fa9a37db4c2816ea3fd624e96f59080e
WoodNeck <saiyu915@naver.com>
2018-05-02 20:48:04 +0900

    fix: Test.sh if-else fix

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Running test on branch: " $TRAVIS_BRANCH
-if [ $TRAVIS_BRANCH == 'devel_fe' ]; then
+echo "Running test on branch:" $TRAVIS_BRANCH
+if [[ $TRAVIS_BRANCH == 'devel_fe' ]]; then
   npm run test_fe
-elif [ $TRAVIS_BRANCH == 'devel_be' ]; then
+elif [[ $TRAVIS_BRANCH == 'devel_be' ]]; then
   python3 manage.py test
 else
   npm run test_all
