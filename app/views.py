77a5e5cea586b01f42bf3c02a216f2796f898ad1
YunSunLee <stulie@snu.ac.kr>
2018-05-03 17:45:05 +0900

    feat(be): implemented Users, Clothes, Tag views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,39 @@
-from app.models import MxM, Reply, Rating
-from app.serializers import MxMSerializer, ReplySerializer, RatingSerializer
+from app.models import Clothes, Tag, MxM, Reply, Rating
+from app.serializers import (
+    UserSerializer, ClothesSerializer, TagSerializer, 
+    MxMSerializer, ReplySerializer, RatingSerializer
+)
+from django.contrib.auth.models import User
 from rest_framework import generics
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ClothesList(generics.ListCreateAPIView):
+    queryset = Clothes.objects.all()
+    serializer_class = ClothesSerializer
+
+
+class ClothesDetail(generics.RetrieveAPIView):
+    queryset = Clothes.objects.all()
+    serializer_class = ClothesSerializer
+
+
+class TagList(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
 
 class MxMList(generics.ListAPIView):
