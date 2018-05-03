8a14550e85d3fcd9d619720ef7ccced1f47087a5
YunSunLee <stulie@snu.ac.kr>
2018-05-03 17:41:56 +0900

    feat(be): implemented Users, Clothes, Tag

--- app/models.py
+++ app/models.py
@@ -1,7 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
+class Clothes(models.Model):
+    owner = models.ForeignKey(
+        'auth.User', related_name = 'clothes',
+        on_delete=models.CASCADE, default = 1
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(default='media/default_image.jpeg')
+    is_wildcard = models.BooleanField(default=False)
+    tag = models.ManyToManyField('Tag')
+
+
+class Tag(models.Model):
+    content = models.CharField(max_length=100, default='')
+    TYPE_CHOICES = (
+        ('Color','Color'),('Season', 'Season'),
+        ('Texture','Texture'), ('Category','Category')
+    )
+    type = models.CharField(max_length=100, choices=TYPE_CHOICES, default='Color')
+
+    def __str__(self):
+        return '{type}:{content}'.format(
+            type=self.type, content=self.content
+        )
 
 class MxM(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
