4c131e76867da487e3c1ffc8f548feac11e4f991
WoodNeck <saiyu915@naver.com>
2018-05-03 15:32:32 +0900

    feat(fe): Vuex 및 Axios 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
+Vue.use(Vuex)
 Vue.use(Router)
 
 export default new Router({
