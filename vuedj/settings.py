748b3402158688181042389bb1a1650deeafc1c5
YunSunLee <stulie@snu.ac.kr>
2018-05-13 19:26:49 +0900

    feat(be): pagination

--- vuedj/settings.py
+++ vuedj/settings.py
@@ -167,3 +167,8 @@ NOSE_ARGS = [
 # Image
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
+}
