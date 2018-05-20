dea975c306618f663c7e23adadcbe5223ae22e63
kj4569 <kj4569@snu.ac.kr>
2018-05-20 22:25:25 +0900

    refactor(fe): All Clothes vuex 적용

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import navbar from './modules/navbar'
 import closet from './modules/closet'
+import allClothes from './modules/allClothes'
 
 Vue.use(Vuex)
 
@@ -10,7 +11,8 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
   modules: {
     navbar,
-    closet
+    closet,
+    allClothes
   },
   strict: debug
 })
