c6959b5614d49d823cbd22680afebba5b479083d
yskim5892 <yskim5892@gmail.com>
2018-05-29 21:12:36 +0900

    feat(fe): filter button added in MxM Plaza Page

--- src/components/MxMPlaza.vue
+++ src/components/MxMPlaza.vue
@@ -9,11 +9,13 @@
       </h2>
       <br/>
     </div>
-
+    <button v-on:click="filterAll" :class="{ active: filter == 'all' }"> all</button>
+    <button v-on:click="filterRecom" :class="{ active: filter == 'recommendation' }"> recommendation</button>
+    <button v-on:click="filterEval" :class="{ active: filter == 'evaluation' }"> evaluation</button>
     <div class="container">
       <div id="mxm-plaza-wrapper">
         <ul>
-          <li v-for="mxm in mxms">
+          <li v-for="mxm in filteredMxMs">
             {{mxm.id}}<br/>
             <div class="row">
               <span v-for="cloth in mxm.clothes">
@@ -32,9 +34,34 @@ export default {
   created () {
     this.$store.dispatch('MXMPLAZA_LOAD')
   },
+  data () {
+    return {
+      filter: 'all'
+    }
+  },
   computed: {
-    mxms () {
+    filteredMxMs () {
+      return this[this.filter]
+    },
+    all () {
       return this.$store.getters.mxmsInPlaza
+    },
+    recommendation () {
+      return this.all.filter((mxm) => mxm.is_on_recommendation)
+    },
+    evaluation () {
+      return this.all.filter((mxm) => mxm.is_on_evaluation)
+    }
+  },
+  methods: {
+    filterAll () {
+      this.filter = 'all'
+    },
+    filterRecom () {
+      this.filter = 'recommendation'
+    },
+    filterEval () {
+      this.filter = 'evaluation'
     }
   }
 }
